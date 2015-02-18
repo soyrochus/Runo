c2f94a769c8312d662d584c3494a3766bed0900a
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2015-02-18 09:30:22 +0100

    First commit; creation of project based on http://fsprojects.github.io/ProjectScaffold

---
+++ src/runo/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("runo")>]
+[<assembly: AssemblyProductAttribute("runo")>]
+[<assembly: AssemblyDescriptionAttribute("Runo ('runes') - Magic words for F#")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
